c56c1542b67eb2b22c5c27d17c89abed0e7dd92a
hector <hector@shipedge.com>
2024-09-12 17:36:57 -0400

    refactor

--- init.lua
+++ init.lua
@@ -791,6 +791,7 @@ require("lazy").setup({
 	-- 		vim.cmd.hi("Comment gui=none")
 	-- 	end,
 	-- },
+	-- Everforest theme
 	-- {
 	-- 	"sainnhe/everforest",
 	-- 	lazy = false,
@@ -802,6 +803,7 @@ require("lazy").setup({
 	-- 		vim.cmd.colorscheme("everforest")
 	-- 	end,
 	-- },
+	-- Sonokai theme
 	-- {
 	-- 	"sainnhe/sonokai",
 	-- 	lazy = false,
@@ -847,38 +849,41 @@ require("lazy").setup({
 	--   end,
 	-- },
 	-- Kanagawa theme
-	{
-		"rebelot/kanagawa.nvim",
-		config = function() -- Default options:
-			require("kanagawa").setup({
-				compile = false, -- enable compiling the colorscheme
-				undercurl = true, -- enable undercurls
-				commentStyle = { italic = true },
-				functionStyle = {},
-				keywordStyle = { italic = true },
-				statementStyle = { bold = true },
-				typeStyle = {},
-				transparent = false, -- do not set background color
-				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-				terminalColors = true, -- define vim.g.terminal_color_{0,17}
-				colors = { -- add/modify theme and palette colors
-					palette = {},
-					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-				},
-				overrides = function(colors) -- add/modify highlights
-					return {}
-				end,
-				theme = "wave", -- Load "wave" theme when 'background' option is not set
-				background = { -- map the value of 'background' option to a theme
-					dark = "wave", -- try "dragon" !
-					light = "lotus",
-				},
-			})
-
-			-- setup must be called before loading
-			vim.cmd("colorscheme kanagawa-dragon")
-		end,
-	},
+	-- {
+	-- 	"rebelot/kanagawa.nvim",
+	-- 	config = function() -- Default options:
+	-- 		require("kanagawa").setup({
+	-- 			compile = false, -- enable compiling the colorscheme
+	-- 			undercurl = true, -- enable undercurls
+	-- 			commentStyle = { italic = true },
+	-- 			functionStyle = {},
+	-- 			keywordStyle = { italic = true },
+	-- 			statementStyle = { bold = true },
+	-- 			typeStyle = {},
+	-- 			transparent = true, -- do not set background color
+	-- 			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+	-- 			terminalColors = true, -- define vim.g.terminal_color_{0,17}
+	-- 			colors = { -- add/modify theme and palette colors
+	-- 				palette = {},
+	-- 				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+	-- 			},
+	-- 			overrides = function(colors) -- add/modify highlights
+	-- 				return {}
+	-- 			end,
+	-- 			theme = "wave", -- Load "wave" theme when 'background' option is not set
+	-- 			background = { -- map the value of 'background' option to a theme
+	-- 				dark = "wave", -- try "dragon" !
+	-- 				light = "lotus",
+	-- 			},
+	-- 			options = {
+	-- 				transparent_mode = true,
+	-- 			},
+	-- 		})
+	--
+	-- 		-- setup must be called before loading
+	-- 		vim.cmd("colorscheme kanagawa-dragon")
+	-- 	end,
+	-- },
 	-- Agregar one-vim theme
 	-- {
 	-- 	"rakr/vim-one",
@@ -899,23 +904,23 @@ require("lazy").setup({
 	-- 	end,
 	-- },
 	-- Catppucin theme
-	-- {
-	--   "catppuccin/nvim",
-	--   name = "catppuccin",
-	--   config = function()
-	--     require("catppuccin").setup({
-	--       flavour = "mocha", -- Puedes cambiar a 'latte', 'frappe', 'macchiato', o 'mocha'
-	--       integrations = {
-	--         nvimtree = true,
-	--         telescope = true,
-	--         -- Elimina la l’nea de lualine si da problemas
-	--         -- lualine = true,
-	--         -- Habilita m‡s integraciones si es necesario
-	--       },
-	--     })
-	--     vim.cmd("colorscheme catppuccin")
-	--   end,
-	-- },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- Puedes cambiar a 'latte', 'frappe', 'macchiato', o 'mocha'
+				integrations = {
+					nvimtree = true,
+					telescope = true,
+					-- Elimina la l’nea de lualine si da problemas
+					-- lualine = true,
+					-- Habilita m‡s integraciones si es necesario
+				},
+			})
+			vim.cmd("colorscheme catppuccin")
+		end,
+	},
 	-- {
 	-- 	"navarasu/onedark.nvim",
 	-- 	config = function()
