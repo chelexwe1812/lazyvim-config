92777f50d7b25817c36b0e0d57e31135b45bcba7
hector <hector@shipedge.com>
2024-09-19 07:15:07 -0400

    refactor

--- init.lua
+++ init.lua
@@ -855,21 +855,21 @@ require("lazy").setup({
 	-- 	end,
 	-- },
 	-- NOTE Agrega Nightfox theme
-	-- {
-	-- 	"EdenEast/nightfox.nvim",
-	-- 	config = function()
-	-- 		-- Configura el tema aqu’ si es necesario
-	-- 		require("nightfox").setup({
-	-- 			options = {
-	-- 				-- Opciones de configuraci—n del tema
-	-- 				transparent_mode = true,
-	-- 			},
-	-- 		})
-	--
-	-- 		-- Establecer la variante Nordfox
-	-- 		vim.cmd("colorscheme nordfox")
-	-- 	end,
-	-- },
+	{
+		"EdenEast/nightfox.nvim",
+		config = function()
+			-- Configura el tema aqu’ si es necesario
+			require("nightfox").setup({
+				options = {
+					-- Opciones de configuraci—n del tema
+					transparent_mode = true,
+				},
+			})
+
+			-- Establecer la variante Nordfox
+			vim.cmd("colorscheme nordfox")
+		end,
+	},
 	-- Flow colorshceme
 	-- {
 	--   "0xstepit/flow.nvim",
@@ -887,43 +887,43 @@ require("lazy").setup({
 	--     vim.cmd "colorscheme flow"
 	--   end,
 	-- },
-	-- Kanagawa theme
-	{
-		"rebelot/kanagawa.nvim",
-		config = function() -- Default options:
-			require("kanagawa").setup({
-				compile = false, -- enable compiling the colorscheme
-				undercurl = true, -- enable undercurls
-				commentStyle = { italic = true },
-				functionStyle = {},
-				keywordStyle = { italic = true },
-				statementStyle = { bold = true },
-				typeStyle = {},
-				transparent = true, -- do not set background color
-				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-				terminalColors = true, -- define vim.g.terminal_color_{0,17}
-				colors = { -- add/modify theme and palette colors
-					palette = {},
-					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-				},
-				overrides = function(colors) -- add/modify highlights
-					return {}
-				end,
-				theme = "wave", -- Load "wave" theme when 'background' option is not set
-				background = { -- map the value of 'background' option to a theme
-					dark = "wave", -- try "dragon" !
-					light = "lotus",
-				},
-				options = {
-					transparent_mode = true,
-				},
-			})
-
-			-- setup must be called before loading
-			vim.cmd("colorscheme kanagawa-dragon")
-		end,
-	},
-	-- Agregar one-vim theme
+	-- NOTE: Kanagawa theme
+	-- {
+	-- 	"rebelot/kanagawa.nvim",
+	-- 	config = function() -- Default options:
+	-- 		require("kanagawa").setup({
+	-- 			compile = false, -- enable compiling the colorscheme
+	-- 			undercurl = true, -- enable undercurls
+	-- 			commentStyle = { italic = true },
+	-- 			functionStyle = {},
+	-- 			keywordStyle = { italic = true },
+	-- 			statementStyle = { bold = true },
+	-- 			typeStyle = {},
+	-- 			transparent = true, -- do not set background color
+	-- 			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+	-- 			terminalColors = true, -- define vim.g.terminal_color_{0,17}
+	-- 			colors = { -- add/modify theme and palette colors
+	-- 				palette = {},
+	-- 				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+	-- 			},
+	-- 			overrides = function(colors) -- add/modify highlights
+	-- 				return {}
+	-- 			end,
+	-- 			theme = "wave", -- Load "wave" theme when 'background' option is not set
+	-- 			background = { -- map the value of 'background' option to a theme
+	-- 				dark = "wave", -- try "dragon" !
+	-- 				light = "lotus",
+	-- 			},
+	-- 			options = {
+	-- 				transparent_mode = true,
+	-- 			},
+	-- 		})
+	--
+	-- 		-- setup must be called before loading
+	-- 		vim.cmd("colorscheme kanagawa-dragon")
+	-- 	end,
+	-- },
+	-- NOTE: one-vim theme
 	-- {
 	-- 	"rakr/vim-one",
 	-- 	config = function()
@@ -931,7 +931,8 @@ require("lazy").setup({
 	-- 		vim.g.one_allow_italics = 1
 	-- 		vim.cmd("colorscheme one")
 	-- 	end,
-	-- },-- Agregar Gruvbox theme
+	-- },
+	-- NOTE: Gruvbox theme
 	-- {
 	-- 	"ellisonleao/gruvbox.nvim",
 	-- 	config = function()
@@ -942,7 +943,7 @@ require("lazy").setup({
 	-- 		vim.cmd("colorscheme gruvbox")
 	-- 	end,
 	-- },
-	-- Catppucin theme
+	-- NOTE: Catppucin theme
 	-- {
 	-- 	"catppuccin/nvim",
 	-- 	name = "catppuccin",
@@ -969,7 +970,7 @@ require("lazy").setup({
 	-- 		require("onedark").load()
 	-- 	end,
 	-- },
-	-- Nordic theme config
+	-- NOTE: Nordic theme config
 	-- {
 	-- 	"AlexvZyl/nordic.nvim",
 	-- 	lazy = false,
