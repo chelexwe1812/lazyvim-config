c0030074c10908118ef1d2f169aecff707af0796
Hector Flores <52297636+chelexwe1812@users.noreply.github.com>
2024-11-18 12:50:45 -0400

    kawagamascheme

--- init.lua
+++ init.lua
@@ -53,7 +53,7 @@ vim.g.maplocalleader = " "
 --
 -- Cargar el esquema de colores
 -- vim.cmd("colorscheme xcode_dark")
-require('themes.tomorrow_night_eighties').setup()
+-- require('themes.tomorrow_night_eighties').setup()
 
 -- Otras configuraciones pueden ir aquí
 -- Set to true if you have a Nerd Font installed and selected in the terminal
@@ -910,41 +910,41 @@ require("lazy").setup({
 	-- 	end,
 	-- },
 	-- NOTE: Kanagawa theme
-	-- {
-	-- 	"rebelot/kanagawa.nvim",
-	-- 	config = function() -- Default options:
-	-- 		require("kanagawa").setup({
-	-- 			compile = false, -- enable compiling the colorscheme
-	-- 			undercurl = true, -- enable undercurls
-	-- 			commentStyle = { italic = true },
-	-- 			functionStyle = {},
-	-- 			keywordStyle = { italic = true },
-	-- 			statementStyle = { bold = true },
-	-- 			typeStyle = {},
-	-- 			transparent = true, -- do not set background color
-	-- 			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-	-- 			terminalColors = true, -- define vim.g.terminal_color_{0,17}
-	-- 			colors = { -- add/modify theme and palette colors
-	-- 				palette = {},
-	-- 				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-	-- 			},
-	-- 			overrides = function(colors) -- add/modify highlights
-	-- 				return {}
-	-- 			end,
-	-- 			theme = "wave", -- Load "wave" theme when 'background' option is not set
-	-- 			background = { -- map the value of 'background' option to a theme
-	-- 				dark = "wave", -- try "dragon" !
-	-- 				light = "lotus",
-	-- 			},
-	-- 			options = {
-	-- 				transparent_mode = true,
-	-- 			},
-	-- 		})
-	--
-	-- 		-- setup must be called before loading
-	-- 		vim.cmd("colorscheme kanagawa-dragon")
-	-- 	end,
-	-- },
+	{
+		"rebelot/kanagawa.nvim",
+		config = function() -- Default options:
+			require("kanagawa").setup({
+				compile = false, -- enable compiling the colorscheme
+				undercurl = true, -- enable undercurls
+				commentStyle = { italic = true },
+				functionStyle = {},
+				keywordStyle = { italic = true },
+				statementStyle = { bold = true },
+				typeStyle = {},
+				transparent = true, -- do not set background color
+				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+				terminalColors = true, -- define vim.g.terminal_color_{0,17}
+				colors = { -- add/modify theme and palette colors
+					palette = {},
+					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+				},
+				overrides = function(colors) -- add/modify highlights
+					return {}
+				end,
+				theme = "wave", -- Load "wave" theme when 'background' option is not set
+				background = { -- map the value of 'background' option to a theme
+					dark = "wave", -- try "dragon" !
+					light = "lotus",
+				},
+				options = {
+					transparent_mode = true,
+				},
+			})
+
+			-- setup must be called before loading
+			vim.cmd("colorscheme kanagawa-dragon")
+		end,
+	},
 	-- NOTE: one-vim theme
 	-- {
 	-- 	"rakr/vim-one",
