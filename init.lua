66c6e5f16aa003444df09e01cc720454fba31c33
Hector Flores <52297636+chelexwe1812@users.noreply.github.com>
2024-11-25 17:12:23 -0400

    blood

--- init.lua
+++ init.lua
@@ -286,99 +286,74 @@ require("lazy").setup({
 			})
 		end,
 	},
+	-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
+	--
+	-- This is often very useful to both group configuration, as well as handle
+	-- lazy loading plugins that don't need to be loaded immediately at startup.
+	--
+	-- For example, in the following configuration, we use:
+	--  event = 'VimEnter'
+	--
+	-- which loads which-key before all the UI elements are loaded. Events can be
+	-- normal autocommands events (`:help autocmd-events`).
+	--
+	-- Then, because we use the `config` key, the configuration only runs
+	-- after the plugin has been loaded:
+	--  config = function() ... end
 
-	-- NOTE: Plugin para reslatar y resolver conflictos
-
--- 	require("lazy")
--- 		.setup({
--- 			{
--- 				"rhysd/conflict-marker.vim",
--- 				config = function()
--- 					-- Configuración opcional, si deseas personalizar mapeos o opciones
--- 					vim.g.conflict_marker_enable_mappings = 1 -- Habilitar mapeos por defecto
--- 					-- Puedes personalizar los colores de los marcadores de conflicto aquí si lo deseas
--- 				end,
--- 			},
--- 		})
--- 		require('lazy').setup({
---     {
---         'rhysd/conflict-marker.vim',
---         config = function()
---             -- Configuración opcional, si deseas personalizar mapeos o opciones
---             vim.g.conflict_marker_enable_mappings = 1 -- Habilitar mapeos por defecto
---             -- Puedes personalizar los colores de los marcadores de conflicto aquí si lo deseas
---         end,
---     },
--- })
-(
-		-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
-		--
-		-- This is often very useful to both group configuration, as well as handle
-		-- lazy loading plugins that don't need to be loaded immediately at startup.
-		--
-		-- For example, in the following configuration, we use:
-		--  event = 'VimEnter'
-		--
-		-- which loads which-key before all the UI elements are loaded. Events can be
-		-- normal autocommands events (`:help autocmd-events`).
-		--
-		-- Then, because we use the `config` key, the configuration only runs
-		-- after the plugin has been loaded:
-		--  config = function() ... end
-
-{ -- Useful plugin to show you pending keybinds.
-			"folke/which-key.nvim",
-			event = "VimEnter", -- Sets the loading event to 'VimEnter'
-			opts = {
-				icons = {
-					-- set icon mappings to true if you have a Nerd Font
-					mappings = vim.g.have_nerd_font,
-					-- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-					-- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
-					keys = vim.g.have_nerd_font and {} or {
-						Up = "<Up> ",
-						Down = "<Down> ",
-						Left = "<Left> ",
-						Right = "<Right> ",
-						C = "<C-â€¦> ",
-						M = "<M-â€¦> ",
-						D = "<D-â€¦> ",
-						S = "<S-â€¦> ",
-						CR = "<CR> ",
-						Esc = "<Esc> ",
-						ScrollWheelDown = "<ScrollWheelDown> ",
-						ScrollWheelUp = "<ScrollWheelUp> ",
-						NL = "<NL> ",
-						BS = "<BS> ",
-						Space = "<Space> ",
-						Tab = "<Tab> ",
-						F1 = "<F1>",
-						F2 = "<F2>",
-						F3 = "<F3>",
-						F4 = "<F4>",
-						F5 = "<F5>",
-						F6 = "<F6>",
-						F7 = "<F7>",
-						F8 = "<F8>",
-						F9 = "<F9>",
-						F10 = "<F10>",
-						F11 = "<F11>",
-						F12 = "<F12>",
-					},
-				},
-
-				-- Document existing key chains
-				spec = {
-					{ "<leader>c", group = "[C]ode", mode = { "n", "x" } },
-					{ "<leader>d", group = "[D]ocument" },
-					{ "<leader>r", group = "[R]ename" },
-					{ "<leader>s", group = "[S]earch" },
-					{ "<leader>w", group = "[W]orkspace" },
-					{ "<leader>t", group = "[T]oggle" },
-					{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+	{ -- Useful plugin to show you pending keybinds.
+		"folke/which-key.nvim",
+		event = "VimEnter", -- Sets the loading event to 'VimEnter'
+		opts = {
+			icons = {
+				-- set icon mappings to true if you have a Nerd Font
+				mappings = vim.g.have_nerd_font,
+				-- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+				-- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
+				keys = vim.g.have_nerd_font and {} or {
+					Up = "<Up> ",
+					Down = "<Down> ",
+					Left = "<Left> ",
+					Right = "<Right> ",
+					C = "<C-â€¦> ",
+					M = "<M-â€¦> ",
+					D = "<D-â€¦> ",
+					S = "<S-â€¦> ",
+					CR = "<CR> ",
+					Esc = "<Esc> ",
+					ScrollWheelDown = "<ScrollWheelDown> ",
+					ScrollWheelUp = "<ScrollWheelUp> ",
+					NL = "<NL> ",
+					BS = "<BS> ",
+					Space = "<Space> ",
+					Tab = "<Tab> ",
+					F1 = "<F1>",
+					F2 = "<F2>",
+					F3 = "<F3>",
+					F4 = "<F4>",
+					F5 = "<F5>",
+					F6 = "<F6>",
+					F7 = "<F7>",
+					F8 = "<F8>",
+					F9 = "<F9>",
+					F10 = "<F10>",
+					F11 = "<F11>",
+					F12 = "<F12>",
 				},
 			},
-		}),
+
+			-- Document existing key chains
+			spec = {
+				{ "<leader>c", group = "[C]ode", mode = { "n", "x" } },
+				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>r", group = "[R]ename" },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>w", group = "[W]orkspace" },
+				{ "<leader>t", group = "[T]oggle" },
+				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+			},
+		},
+	},
 
 	-- NOTE: Plugins can specify dependencies.
 	--
@@ -694,17 +669,16 @@ require("lazy").setup({
 				},
 			}
 
-      require('lazy').setup({
-        {
-          'rhysd/conflict-marker.vim',
-          config = function()
-            -- Configuración opcional, si deseas personalizar mapeos o opciones
-            vim.g.conflict_marker_enable_mappings = 1 -- Habilitar mapeos por defecto
-            -- Puedes personalizar los colores de los marcadores de conflicto aquí si lo deseas
-          end,
-        },
-      })
-
+			require("lazy").setup({
+				{
+					"rhysd/conflict-marker.vim",
+					config = function()
+						-- Configuración opcional, si deseas personalizar mapeos o opciones
+						vim.g.conflict_marker_enable_mappings = 1 -- Habilitar mapeos por defecto
+						-- Puedes personalizar los colores de los marcadores de conflicto aquí si lo deseas
+					end,
+				},
+			})
 
 			-- Ensure the servers and tools above are installed
 			--  To check the current status of installed tools and/or manually install
