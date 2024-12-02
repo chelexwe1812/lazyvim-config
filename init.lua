373a294e6c50604d73b8a0ddb18ece279976409a
Hector Flores <52297636+chelexwe1812@users.noreply.github.com>
2024-12-02 10:05:32 -0400

    Mariana

--- init.lua
+++ init.lua
@@ -934,41 +934,41 @@ require("lazy").setup({
 	-- 	end,
 	-- },
 	-- NOTE: Kanagawa theme
-	{
-		"rebelot/kanagawa.nvim",
-		config = function() -- Default options:
-			require("kanagawa").setup({
-				compile = false, -- enable compiling the colorscheme
-				undercurl = true, -- enable undercurls
-				commentStyle = { italic = true },
-				functionStyle = {},
-				keywordStyle = { italic = true },
-				statementStyle = { bold = true },
-				typeStyle = {},
-				transparent = true, -- do not set background color
-				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-				terminalColors = true, -- define vim.g.terminal_color_{0,17}
-				colors = { -- add/modify theme and palette colors
-					palette = {},
-					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-				},
-				overrides = function(colors) -- add/modify highlights
-					return {}
-				end,
-				theme = "wave", -- Load "wave" theme when 'background' option is not set
-				background = { -- map the value of 'background' option to a theme
-					dark = "wave", -- try "dragon" !
-					light = "lotus",
-				},
-				options = {
-					transparent_mode = true,
-				},
-			})
-
-			-- setup must be called before loading
-			vim.cmd("colorscheme kanagawa-dragon")
-		end,
-	},
+	-- {
+	-- 	"rebelot/kanagawa.nvim",
+	-- 	config = function() -- Default options:
+	-- 		require("kanagawa").setup({
+	-- 			compile = false, -- enable compiling the colorscheme
+	-- 			undercurl = true, -- enable undercurls
+	-- 			commentStyle = { italic = true },
+	-- 			functionStyle = {},
+	-- 			keywordStyle = { italic = true },
+	-- 			statementStyle = { bold = true },
+	-- 			typeStyle = {},
+	-- 			transparent = true, -- do not set background color
+	-- 			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+	-- 			terminalColors = true, -- define vim.g.terminal_color_{0,17}
+	-- 			colors = { -- add/modify theme and palette colors
+	-- 				palette = {},
+	-- 				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+	-- 			},
+	-- 			overrides = function(colors) -- add/modify highlights
+	-- 				return {}
+	-- 			end,
+	-- 			theme = "wave", -- Load "wave" theme when 'background' option is not set
+	-- 			background = { -- map the value of 'background' option to a theme
+	-- 				dark = "wave", -- try "dragon" !
+	-- 				light = "lotus",
+	-- 			},
+	-- 			options = {
+	-- 				transparent_mode = true,
+	-- 			},
+	-- 		})
+	--
+	-- 		-- setup must be called before loading
+	-- 		vim.cmd("colorscheme kanagawa-dragon")
+	-- 	end,
+	-- },
 	-- NOTE: one-vim theme
 	-- {
 	-- 	"rakr/vim-one",
@@ -1063,16 +1063,16 @@ require("lazy").setup({
 	-- 	end,
 	-- },
 	-- NOTE: Juliana theme
-	-- {
-	-- 	"kaiuri/nvim-juliana",
-	-- 	lazy = false,
-	-- 	opts = { --[=[ configuration --]=]
-	-- 	},
-	-- 	-- config = true,
-	-- 	config = function()
-	-- 		vim.cmd("colorscheme juliana")
-	-- 	end,
-	-- },
+	{
+		"kaiuri/nvim-juliana",
+		lazy = false,
+		opts = { --[=[ configuration --]=]
+		},
+		-- config = true,
+		config = function()
+			vim.cmd("colorscheme juliana")
+		end,
+	},
 	-- NOTE: VS code theme
 	-- {
 	-- 	"Mofiqul/vscode.nvim",
